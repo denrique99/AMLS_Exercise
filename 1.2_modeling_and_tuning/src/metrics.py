1f59ca6b7925b61f8b77f1ac72a4b7dfcd0b2d9d
Andrea Rachetta <andrea.rachetta@ibm.com>
2025-07-08 21:32:09 +0200

    AugmentationTry

--- 1.2_modeling_and_tuning/src/metrics.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import classification_report, confusion_matrix, balanced_accuracy_score
-
-def print_metrics(y_true, y_pred):
-    print(classification_report(y_true, y_pred, digits=4))
-    print("Confusion Matrix:\n", confusion_matrix(y_true, y_pred))
-    print("Balanced Accuracy:", balanced_accuracy_score(y_true, y_pred))

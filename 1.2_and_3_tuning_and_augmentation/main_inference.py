8264f8cce973d8c702d837585faf4c0f1af8a3a5
denrique99 <dennis.gehrmann@st.ovgu.de>
2025-07-15 19:43:48 +0200

    cleaned inference

--- 1.2_and_3_tuning_and_augmentation/main_inference.py
+++ 1.2_and_3_tuning_and_augmentation/main_inference.py
@@ -1,9 +1,12 @@
 from src.inference import run_inference
+import os
 
 if __name__ == "__main__":
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
     run_inference(
-        model_path="models/pipeline_models/best_lr0.001_wd0.0001_adam_ep100_after_meeting.pth",
+        model_path=os.path.join(base_dir, "models", "pipeline_models", "best_lr0.001_wd0.0001_adam_ep100.pth"),
         test_zip_path="data/X_test.zip",
-        output_path="data/base.csv",
+        output_path=os.path.join(base_dir, "data", "base.csv"),
         batch_size=32
     )
